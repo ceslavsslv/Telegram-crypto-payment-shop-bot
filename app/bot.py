51a74eef00d842bca5bda0405ca26381c164145c
ceslavsslv <cubars42@gmail.com>
2025-07-11 03:32:21 +0300

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -4,9 +4,6 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from app.config import API_TOKEN
 from app.handlers import start, shop, payment, account, support, admin, referral
 
-bot = Bot(token=API_TOKEN, parse_mode="HTML")
-dp = Dispatcher(storage=MemoryStorage())
-
 def register_routers(dispatcher: Dispatcher):
     dispatcher.include_routers(
         start.router,
